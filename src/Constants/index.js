80e312b7bf071d68e73ff6c2cb489ac51ac77354
Adam Lazat <adamdeveloper29@gmail.com>
2024-07-23 18:45:44 +0100

    create constants

---
+++ src/Constants/index.js
@@ -0,0 +1,63 @@
+// Header Section
+export const NAV_MENU_ITEMS = ["home","about","portfolio","contact"]
+
+// Hero Section
+export const JUMBOTRON_CONTENT = {
+    welcomeMsg: "hello, i am",
+    fullName: "mohammed adam lazazat",
+    profileDescrirption: `a frontend developer with 2 years of experience working 
+                         at building user interfaces using HTML/CSS/JavaScript/React.`
+}
+
+// About Section
+export const ABOUT_CONTENT = {
+    aboutImg: "",
+    aboutDetails: {
+        aboutMe: {
+            title: "",
+            text: "",
+            aboutSkills: {
+                title: "",
+                text: "",
+                mySkillsIcons: []
+            }
+        }
+    }
+}
+
+// Portfolio Section
+const PORTFOLIO_FILTERS = ["all","html","css","javascript","react"]
+
+const PORTFOLIO_ITEMS = [
+    {
+        id: 1,
+        img: "",
+        title: "",
+        description: "",
+        links: {},
+        tags: ["html","css"]
+    },
+    {
+        id: 2,
+        img: "",
+        title: "",
+        description: "",
+        links: {},
+        tags: ["html","css"]
+    },
+    {
+        id: 3,
+        img: "",
+        title: "",
+        description: "",
+        links: {},
+        tags: ["html","css"]
+    }
+]
+
+// Contact Section
+export const CONTACT_INFOS = {
+    phoneNumberContact: {icon: "", title: "", phoneNumber: ""},
+    loctionContact: {icon: "", title: "", location: ""},
+    emailContact: {icon: "", title: "", email: ""}
+}
